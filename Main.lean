e5a554554225ea46ce3f8a116c8914f59ce1c333
Eduardo Sandalo Porto <edu@sandalo.dev>
2026-06-14 15:05:33 -0300

    Proofs implemented

---
+++ Main.lean
@@ -0,0 +1,5 @@
+import SmiraLean
+
+def main : IO Unit := do
+  IO.println "If this compiled, the proofs successfully type-checked."
+  IO.println "In the future, we might implement execution."
---
+++ README.md
@@ -0,0 +1,1 @@
+# smira-lean
